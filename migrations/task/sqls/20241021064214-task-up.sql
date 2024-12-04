654ff5d001053c9e5dfe536f596d61e4cd903f77
Diyo11 <yspm.lhung@gmail.com>
2024-12-04 23:10:38 +0800

    update_2.2

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -79,12 +79,12 @@ INSERT INTO "CREDIT_PURCHASE" (user_id, credit_package_id, purchased_credits, pr
     (SELECT credit_amount FROM "CREDIT_PACKAGE" WHERE name = '14 堂組合包方案'),
     (SELECT price FROM "CREDIT_PACKAGE" WHERE name = '14 堂組合包方案')),
     --
-    ((SELECT id FROM "USER" WHERE name = '王小明'),,
+    ((SELECT id FROM "USER" WHERE name = '王小明'),
     (SELECT id FROM "CREDIT_PACKAGE" WHERE name = '21 堂組合包方案'),
     (SELECT credit_amount FROM "CREDIT_PACKAGE" WHERE name = '21 堂組合包方案'),
     (SELECT price FROM "CREDIT_PACKAGE" WHERE name = '21 堂組合包方案')),
     --
-    ((SELECT id FROM "USER" WHERE name = '好野人'),,
+    ((SELECT id FROM "USER" WHERE name = '好野人'),
     (SELECT id FROM "CREDIT_PACKAGE" WHERE name = '14 堂組合包方案'),
     (SELECT credit_amount FROM "CREDIT_PACKAGE" WHERE name = '14 堂組合包方案'),
     (SELECT price FROM "CREDIT_PACKAGE" WHERE name = '14 堂組合包方案'));
